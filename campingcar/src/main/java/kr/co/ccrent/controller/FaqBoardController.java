c2d3cf28a3f030c1bd6a6eb041f7383c828a91a9
sungbea <yayahaha0416@gmail.com>
2023-02-14 10:18:33 +0900

    자잘한 css 변경

--- campingcar/src/main/java/kr/co/ccrent/controller/FaqBoardController.java
+++ campingcar/src/main/java/kr/co/ccrent/controller/FaqBoardController.java
@@ -28,106 +28,33 @@ FaqBoardService service;
 	
 
 
-@RequestMapping(value = "admin/faqboard/registerForm", method = RequestMethod.GET)
-public void registerGET(FaqBoardDTO boardDTO, Model model) throws Exception {
-	logger.info("regist get");
-}
-
-@RequestMapping(value = "admin/faqboard/register", method = RequestMethod.POST)
-public String registerPOST (FaqBoardDTO boardDTO,RedirectAttributes rttr) throws Exception {
-	logger.info("regist post");
-	logger.info(boardDTO.toString());
-	
-	service.register(boardDTO);
-	
-	
-	rttr.addFlashAttribute("msg","success");
-	return "redirect:listAll";
-	}
-
-@RequestMapping(value = "/admin/faqboard/read", method = RequestMethod.GET)
-public ModelAndView read(@RequestParam("bno")int bno) throws Exception{
-   ModelAndView mav = new ModelAndView();
-   System.out.println("글 번호 : "+bno);
-   FaqBoardDTO boardDTO = service.read(bno);
-   mav.setViewName("/admin/faqboard/read"); //뷰의 이름
-   mav.addObject("board", boardDTO); //뷰로 보낼 데이터 값 ( 변수명 , 변수에넣을 데이터 값)
-   return mav;
-}
-
-@RequestMapping(value = "/admin/faqboard/modifyForm", method = RequestMethod.GET)
-public void modifyGET(int bno, Model model) throws Exception {
-	
-	FaqBoardDTO boardDTO = service.read(bno);
-	model.addAttribute("board", boardDTO);
-	System.out.println("넘어온bno : " + boardDTO.getBno());
-	
-}
-
-@RequestMapping(value = "/admin/faqboard/modify", method = RequestMethod.POST)
-public String modifyPOST(FaqBoardDTO boardDTO, RedirectAttributes rttr) throws Exception {
-	
-	logger.info("mod post........");
-	
-	service.modify(boardDTO);
-	rttr.addFlashAttribute("msg", "success");
-	System.out.println("접근중");
-	return "redirect:listAll?keyword=";
-	
-}
-
-@RequestMapping(value ="/admin/faqboard/remove", method = RequestMethod.GET)
-public String remove(int bno ,RedirectAttributes rttr) throws Exception {
-	
-	service.remove(bno);
-
-	System.out.println("he");
-	
-	return "redirect:/admin/faqboard/listAll";
-}
-
-@GetMapping(value="/admin/faqboard/listAll")
-public void list1(Criteria cri, Model model) throws Exception{
-   System.out.println(cri.toString());
-   model.addAttribute("list", service.faq_get(cri));
-   
-   //페이징처리
-   PageMaker pageMaker = new PageMaker();
-   
-   pageMaker.setCri(cri);
-   pageMaker.setTotalCount(service.faq_get_count(cri));
-   
-   model.addAttribute("cri",cri);
-   model.addAttribute("pageMaker",pageMaker);
-}
-
-
 //faq게시판 글 상세보기
 @RequestMapping(value = "/faqboard/read", method = RequestMethod.GET)
 public ModelAndView read_member(@RequestParam("bno")int bno) throws Exception{
-   ModelAndView mav = new ModelAndView();
-   System.out.println("글 번호 : "+bno);
-   FaqBoardDTO boardDTO = service.read(bno);
-   mav.setViewName("/faqboard/read"); //뷰의 이름
-   mav.addObject("board", boardDTO); //뷰로 보낼 데이터 값 ( 변수명 , 변수에넣을 데이터 값)
-   return mav;
+ ModelAndView mav = new ModelAndView();
+ System.out.println("글 번호 : "+bno);
+ FaqBoardDTO boardDTO = service.read(bno);
+ mav.setViewName("/faqboard/read"); //뷰의 이름
+ mav.addObject("board", boardDTO); //뷰로 보낼 데이터 값 ( 변수명 , 변수에넣을 데이터 값)
+ return mav;
 }
 
 //faq게시판 글 전체보기/검색기능
 @GetMapping(value="/faqboard/listAll")
 public void list_member(Criteria cri, Model model) throws Exception{
-   System.out.println(cri.toString());
-   model.addAttribute("list", service.faq_get(cri));
-   
-   //페이징처리
-   PageMaker pageMaker = new PageMaker();
-   
-   pageMaker.setCri(cri);
-   pageMaker.setTotalCount(service.faq_get_count(cri));
-   
-   model.addAttribute("cri",cri);
-   model.addAttribute("pageMaker",pageMaker);
+ System.out.println(cri.toString());
+ model.addAttribute("list", service.faq_get(cri));
+ 
+ //페이징처리
+ PageMaker pageMaker = new PageMaker();
+ 
+ pageMaker.setCri(cri);
+ pageMaker.setTotalCount(service.faq_get_count(cri));
+ 
+ model.addAttribute("cri",cri);
+ model.addAttribute("pageMaker",pageMaker);
 }	
+
 	
 
 }
